f0e7802dc13457cf8d622bf4248856ead993a167
Sarequl Basar <sarequlbasar@gmail.com>
2022-07-21 21:29:13 +0600

    init release

---
+++ admin/admin_options.php
@@ -0,0 +1,135 @@
+<?php
+add_action( 'admin_menu', 'wpuser_register_csv_add_admin_menu' );
+add_action( 'admin_init', 'wpuser_register_csv_settings_init' );
+
+
+function wpuser_register_csv_add_admin_menu(  ) { 
+
+	add_submenu_page( 'users.php', 'Register From CSV', 'Register From CSV', 'manage_options', 'user_register_from_csv', 'wpuser_register_csv_options_page' );
+
+}
+
+
+function wpuser_register_csv_settings_init(  ) { 
+
+	register_setting( 'wpurc_sarequl', 'wpuser_register_csv_settings' );
+
+	add_settings_section(
+		'wpuser_register_csv_wpurc_sarequl_section', 
+		__( 'Your section description', 'wpurclang' ), 
+		'wpuser_register_csv_settings_section_callback', 
+		'wpurc_sarequl'
+	);
+
+}
+
+
+
+function wpuser_register_csv_settings_section_callback(  ) { 
+
+	echo __( 'User Register Dashboard', 'wpurclang' );
+
+}
+
+
+function wpuser_register_csv_options_page(  ) { 
+
+	?>
+	<style>
+		.csv_btn {
+			margin-top: 15px;
+		}
+		.alart.alart-success {
+			margin: 10px 0px;
+			font-size: 15px;
+			color: #2aaf2a;
+			font-weight: 600;
+			width: 10%;
+		}
+		.wpurc-importer-area b {
+			color: #2271b1;
+		}
+	</style>
+	<div class="wpurc-importer-area">
+		<h2>User Register From CSV - Options</h2>
+	<?php 
+			function wpurc_import_csv($csvfile){
+				$csv_users = array_map('str_getcsv', file($csvfile));
+				//return $csv_users;
+				
+				$new_users_data= []; //empty array to push organized data
+
+				/**
+				 * Loop through csv file and push data to new array
+				 */
+				for ($i=0; $i < count($csv_users); $i++) { 
+
+					if(!empty($csv_users[$i][0])){
+						$new_users_data[$i]['user_login'] = $csv_users[$i][0];
+					}
+
+					if(!empty($csv_users[$i][1])){
+						$new_users_data[$i]['user_email']	= $csv_users[$i][1];
+					}
+
+					if(!empty($csv_users[$i][2])){
+						$new_users_data[$i]['user_pass'] = $csv_users[$i][2];
+					}
+
+					if(!empty($csv_users[$i][3])){
+						$new_users_data[$i]['first_name'] = $csv_users[$i][3];
+					}
+
+					if(!empty($csv_users[$i][4])){
+						$new_users_data[$i]['last_name']	= $csv_users[$i][4];
+					}
+
+					if(!empty($csv_users[$i][5])){
+						$new_users_data[$i]['role']	= $csv_users[$i][5];
+					}
+				}
+
+				/*remove header*/
+				unset($new_users_data[0]); 
+
+				/**
+				 * register user
+				 */
+				foreach($new_users_data as $user){
+					$user_id = wp_insert_user($user);
+
+					if(!is_wp_error($user_id)){
+						echo '<p>User <b>'.$user['user_login'].' </b> created successfully</p>';
+					}
+				}
+				/**
+				 * register user end
+				 */
+			}
+
+
+			if(isset($_POST['wpurc_csv_import_button'])){
+				$csv = $_FILES['csv_file']['tmp_name'];
+				if(!empty($csv)){
+					
+					wpurc_import_csv($csv);
+					echo '<div class="alart alart-success">CSV Import Success!</div>';
+				}
+			}
+		?>
+		
+			<form action="" method="post" enctype="multipart/form-data">
+
+				<div class="ci-form-group"> 
+					<input type="file" class="ci-form-control" name="csv_file" id="">
+				</div>
+				<div class="csv_btn">
+					<input class="wpurc-btn button-primary" type="submit" name="wpurc_csv_import_button" value="Import"> 
+				</div>
+
+			</form>
+		</div>
+		
+		<?php
+
+}
---
+++ wpuser_register_csv.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package wpuser_register_csv
+ * @version 1.0.0
+ */
+/*
+Plugin Name: User Register From CSV
+Plugin URI: https://github.com/sarequl/wpuser-register-csv
+Description: You can use this plugin to register user from csv file. Maybe you have a old website and you want transfer your user from old website to new website. you can use this plugin to register user from csv file.
+Version: 1.0.0
+Author: Sarequl Basar
+Author URI: https://sarequl.me
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use. - https://sarequl.me
+ */
+define( 'wpuser_register_csv', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ */
+function activate_wpuser_register_csv() {
+	
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ */
+function deactivate_wpuser_register_csv() {
+	
+}
+
+function wpuser_register_csv_action_links( $links,$plugin ) {
+
+	$plugin_path = plugin_dir_path(__FILE__);
+
+	$new_link = array();
+	if(basename($plugin_path).'/wpuser_register_csv.php' == $plugin){
+
+		$new_link['settings'] = '<a href="'. esc_url( get_admin_url( null, 'users.php?page=user_register_from_csv' ) ) .'">'. __( 'Settings', 'wpuser_register_csv' ) .'</a>';
+	}
+
+	return array_merge($new_link,$links);
+}
+
+add_filter('plugin_action_links', 'wpuser_register_csv_action_links', 10, 2);
+
+
+
+
+register_activation_hook( __FILE__, 'activate_wpuser_register_csv' );
+register_deactivation_hook( __FILE__, 'deactivate_wpuser_register_csv' );
+
+
+//admin panel
+require plugin_dir_path( __FILE__ ) . 'admin/admin_options.php';
+
+
+
